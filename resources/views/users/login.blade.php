c542361915193c1d2939e2000e6a398628097316
haru034 <haru034@icloud.com>
2022-02-21 20:32:15 +0900

    ログイン画面のViewとCSSファイルを新規作成

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,15 @@
+<!-- ログイン画面のView -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> <!-- bootstrap読み込み -->
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}"> <!-- login.cssと連携 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>新規登録画面</title>
+</head>
+<body>
+    
+</body>
+</html>
