a89254c0c07074b9124eea12e442433373b99785
haru034 <haru034@icloud.com>
2022-02-21 20:46:36 +0900

    認証機能、ホーム画面、チャット機能のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
+<!-- ルーティング -->
 <?php
+
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +17,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//新規登録画面の表示
+Route::get('/signup_form', 'UsersController@signup_form')->name('signup_form');
+Route::post('/signup', 'UsersController@signup')->name('signup'); // 新規登録の処理
+
+//ログイン画面へ遷移
+Route::get('/login_form', 'UsersController@login_form')->name('login_form');
+Route::post('/login', 'UsersController@login')->name('login'); // ログイン認証
+Route::post('/logout', 'UsersController@logout')->name('logout'); // ログアウト
+
+//ホーム画面へ遷移
+Route::get('/home_screen', 'ChatController@home_screen')->name('home_screen');
+
+//チャット登録
+Route::post('/chat', 'ChatController@chat')->name('chat');
+Route::get('/chat_delete/{id}', 'ChatController@chat_delete')->name('chat_delete'); //チャット削除
