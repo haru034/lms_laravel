d33fa8a4c49d2f0254e2add1ce327e2b2ca9d480
haru034 <haru034@icloud.com>
2022-02-22 19:15:42 +0900

    新規登録画面とログイン画面のViewを記述

--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -10,6 +10,29 @@
     <title>新規登録画面</title>
 </head>
 <body>
-    
+    <div class="signup">
+        <div>
+            <h1>新規登録画面</h1>
+        </div>
+            <div class="outer-signup-form">
+                <section>
+                    <form action="{{ route('signup') }}" method="post" enctype="multipart/form-data" autocomplete="off">
+                        @csrf <!-- CSRF保護 -->
+                        <p><input type="text" name="name" placeholder="本名を入力してください" autocomplete="off" style="width:250px; height:30px;"></P>
+                        <p><input type="text" name="nickname" placeholder="ニックネームを入力してください" autocomplete="off" style="width:250px; height:30px; margin-top:10px;"></P>
+                        <p><input type="email" name="email" placeholder="メールアドレスを入力してください" autocomplete="off" style="width:250px; height:30px; margin-top:10px;"></p>
+                        <p><input type="password" name="password" placeholder="パスワードを入力してください" autocomplete="off" style="width:250px; height:30px; margin-top:10px;"></p>
+                        <p><input type="submit" value="登録する" autocomplete="off" style="width:200px; height:30px; margin-top:20px;"></p>
+                    </form>
+                    <a href="{{ route('login_form') }}">ログイン画面に戻る</a> <!-- リダイレクト -->
+                </section>
+            </div>
+        </form>
+        <div class="error-indication">
+            @foreach ($errors->all() as $error) <!--入力ミスまたは重複している部分があれば、警告文で知らせてくれる処理-->
+                <li>{{$error}}</li>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
