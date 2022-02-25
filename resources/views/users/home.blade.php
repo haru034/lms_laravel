216995282a20c0cdcb16ac27c8f094871e928576
haru034 <haru034@icloud.com>
2022-02-26 03:40:11 +0900

    ホーム画面のViewとCSSが完成

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -10,49 +10,49 @@
     <title>ホーム画面</title>
 </head>
 <body>
-    <h1>プログラミング初学者が集う掲示板(β)</h1>
+    <h1>全国のプログラミング初学者が集う掲示板(β)</h1>
     <section>
-        <div class="home">
+        <div class="btn-movement">
             <div>
                 <p><a class="btn btn-primary" href="{{ url('') }}">マイページに移動</a></p>
                 <!-- <p><a class="btn btn-danger" href="{{ route('home_screen') }}">ホーム画面に戻る</a></p> -->
             </div>
+            <div>
                 <form action="{{ route('logout') }}" method="post">
                     @csrf <!-- CSRF保護 -->
                     <input type="submit" value="ログアウト"> <!-- ログアウトしてログイン画面に戻る -->
                 </form>
             </div>
         </div>
-        <div> <!-- チャット一覧画面 -->
+        <div class="chat"> <!-- チャット一覧画面 -->
             <form action="{{ route('chat') }}" method="post" autocomplete="off" style="height:300px">
                 @csrf <!-- CSRF保護 -->
                 <div class="outer-message-form">
                     <nobr>「今から学習を始めます」「終わります」など、学習に関する事なら何でもいいので投稿しましょう！</nobr>
-                    <!-- <textarea name="message" placeholder="ここにメッセージを入力してください" autocomplete="off" rows="30" cols="20"></textarea><br> rows =「高さ」, cols =「幅」-->
                     <textarea name="message" placeholder="ここにメッセージを入力してください" autocomplete="off" rows="3" cols="200"></textarea><br> <!-- rows =「高さ」, cols =「幅」-->
-                    <input type="submit" value="投稿" style=background:#99CC00;>
+                    <button type="submit" class="btn btn-success">投稿</button>
                 </div>
             </form>
-            <div class="chatarea">
-                <table class="st-tbl1">
-                    <thead>
-                        <tr>
-                            <th>表示名</th>
-                            <th>メッセージ</th>
-                            <th>投稿日時</th>
-                        </tr>
-                    </thead>
-                    @foreach ($chats as $chat) <!-- chatテーブルのデータを全て表示させる処理 -->
-                    <tbody>
-                        <tr>
-                            <td style="width:10%">{{ $chat->user->nickname }}</td> <!-- $chatに、user関数を使い、その中のnameを参照 -->
-                            <td style="width:40%">{{ $chat->message }}</td>
-                            <td style="width:20%">{{ $chat->created_at }}</td>
-                        </tr>
-                    </tbody>
-                    @endforeach
-                </table>
-            </div>
+        </div>
+        <div class="chatarea">
+            <table class="st-tbl1">
+                <thead>
+                    <tr>
+                        <th>表示名(ニックネーム)</th>
+                        <th>メッセージ</th>
+                        <th>投稿日時</th>
+                    </tr>
+                </thead>
+                @foreach ($chats as $chat) <!-- chatテーブルのデータを全て表示させる処理 -->
+                <tbody>
+                    <tr>
+                        <td style="width:10%">{{ $chat->user->nickname }}</td> <!-- $chatに、user関数を使い、その中のnicknameを参照 -->
+                        <td style="width:40%">{{ $chat->message }}</td>
+                        <td style="width:20%">{{ $chat->created_at }}</td>
+                    </tr>
+                </tbody>
+                @endforeach
+            </table>
         </div>
     </section>
 </body>
