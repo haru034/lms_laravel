6c2bd9d160bf3dbef75f428045bd15bff2b85f71
haru034 <haru034@icloud.com>
2022-02-28 17:02:52 +0900

    'start_time'、'end_time'カラムを削除

--- database/migrations/2022_02_26_160922_records.php
+++ database/migrations/2022_02_26_160922_records.php
@@ -16,10 +16,8 @@ class Records extends Migration
         Schema::create('records', function (Blueprint $table) {
             $table->increments('id'); // ID(user_id)
             $table->integer('user_id'); // usersテーブルのid
-            $table->datetime('start_time'); // 学習の開始時刻
-            $table->datetime('end_time'); // 学習の終了時刻
             $table->string('lifestyle'); // 生活リズム
-            $table->string('learning_time'); //　学習時間
+            $table->string('learning_time'); //　学習時間の合計
             $table->string('thoughts'); //　感想
         });
     }
