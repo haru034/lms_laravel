9c9988205ee4c0feb6f0ffa2fc611fd0b57e346a
haru034 <haru034@icloud.com>
2022-11-14 21:25:57 +0900

    [update]Recordモデルを作成

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'health', 'kg', 'study', 'thounght'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User'); // usersテーブルと紐づけ
+    }
 }
