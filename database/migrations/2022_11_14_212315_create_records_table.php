979d395c716b8ea9a030e07b88e33e08170bcde5
haru034 <haru034@icloud.com>
2022-11-21 20:38:10 +0900

    [update]recordsテーブルのマイグレーションファイルを作成

--- database/migrations/2022_11_14_212315_create_records_table.php
+++ database/migrations/2022_11_14_212315_create_records_table.php
@@ -14,8 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('records', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+            $table->unsignedInteger('user_id'); // usersテーブルのid
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');  // usersテーブルのidが削除された場合、同じuser_idをrecordsテーブルから削除
+            $table->char('health'); // 生活リズム
+            $table->smallInteger('kg'); // 体重
+            $table->integer('study'); // 学習時間
+            $table->text('thought'); // 感想
+            $table->timestamps(); // 登録・更新日時
         });
     }
 
