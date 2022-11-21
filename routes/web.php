45c29981669e4686ec05c4d87dc70fc14e413a7a
haru034 <haru034@icloud.com>
2022-11-21 23:46:51 +0900

    [update]RecordControllerのuse文を追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\RecordController;
 
 /*
 |--------------------------------------------------------------------------
