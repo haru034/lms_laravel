c4f5a7a747f39795216108dd0f6425a578506afe
haru034 <haru034@icloud.com>
2023-02-02 23:07:13 +0900

    [update] 過去ログ一覧画面を表示するルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -18,43 +18,46 @@ use App\Http\Controllers\RecordController;
 */
 Route::get('/', [UsersController::class, 'login_form']); // php artisan serve起動時に表示
 
-//新規登録画面の表示
+// 新規登録画面の表示
 Route::get('/signup_form', [UsersController::class, 'signup_form'])->name('signup_form');
 
 Route::post('/signup', [UsersController::class, 'signup'])->name('signup'); // 新規登録の処理
 
-//ログイン画面へ遷移
+// ログイン画面へ遷移
 Route::get('/login_form', [UsersController::class, 'login_form'])->name('login_form');
 
 Route::post('/login', [UsersController::class, 'login'])->name('login'); // ログイン認証
 
 
-//ホーム画面へ遷移
+// ホーム画面へ遷移
 Route::get('/home_screen', [ChatController::class, 'home_screen'])->name('home_screen');
 
-//チャット登録
+// チャット登録
 Route::post('/chat', [ChatController::class, 'chat'])->name('chat');
 
-//チャット削除
+// チャット削除
 Route::get('/chat_delete/{id}', [ChatController::class, 'chat_delete'])->name('chat_delete');
 
-//マイページへ遷移
+// マイページへ遷移
 Route::get('/mypage', [UsersController::class, 'mypage'])->name('mypage');
 
-//ユーザー情報の更新
+// ユーザー情報の更新
 Route::post('/update', [UsersController::class, 'update'])->name('users.update');
 
-//ユーザーを削除
+// ユーザーを削除
 Route::post('/destroy', [UsersController::class, 'destroy'])->name('users.destroy');
 
-//学習記録画面へ遷移
+// 学習記録画面へ遷移
 Route::get('/record_screen', [RecordController::class, 'record_screen'])->name('record_screen');
 
-//学習記録を登録
+// 学習記録を登録
 Route::post('/record', [RecordController::class, 'record'])->name('record');
 
-//学習記録編集画面へ遷移
+// 学習記録編集画面へ遷移
 Route::get('/edit_screen', [RecordController::class, 'edit_screen'])->name('edit_screen');
 
-//学習記録を更新
+// 学習記録を更新
 Route::put('/update', [RecordController::class, 'update'])->name('update');
+
+// 過去ログ一覧画面の表示
+Route::get('/list_screen', [RecordController::class, 'list_screen'])->name('list_screen');
