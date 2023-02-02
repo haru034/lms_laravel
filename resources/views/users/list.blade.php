f0cb42ca2f3ab70ad87ae3da7d6ce6d2c4525fb5
haru034 <haru034@icloud.com>
2023-02-02 23:11:26 +0900

    [add] 過去ログ一覧画面のViewファイルを新規作成・編集

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,28 @@
+<!-- 過去ログ一覧画面のView -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> <!-- bootstrap読み込み -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>過去ログ一覧画面</title>
+</head>
+<body>
+    <h1>過去ログ一覧画面</h1>
+    <section>
+        <div class="listarea-btn">
+            <p class="btn-1"><a class="btn btn-primary" href="{{ url('home_screen') }}">ホーム画面に戻る</a></p>
+        </div>
+        @foreach ($posts as $record)
+        <tr>
+            <p class="list_date_title">{{$record->updated_at->format("Y/m/d($week[$date])")}}の記録</p>
+            <td>{{$record->health}}</td><br>
+            <td>{{$record->kg}}</td><br>
+            <td>{{$record->hours}}</td><br>
+            <td>{{$record->thought}}</td><br>
+        </tr>
+        @endforeach
+    </section>
+</body>
+</html>
