dab7e2d9b4142a8403791c761e13543fe52d7f36
haru034 <haru034@icloud.com>
2023-02-02 23:29:47 +0900

    [update] list.css読み込み

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> <!-- bootstrap読み込み -->
+    <link rel="stylesheet" href="{{ asset('css/list.css') }}"> <!-- list.cssと連携 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>過去ログ一覧画面</title>
