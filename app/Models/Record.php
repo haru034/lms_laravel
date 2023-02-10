a1b4d37fe0d77e3f5f59ca3df466e4f87bec90da
haru034 <haru034@icloud.com>
2023-02-11 01:45:46 +0900

    [update] $fillableに'goal'を追加

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -10,7 +10,7 @@ class Record extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id', 'health', 'kg', 'study', 'thounght'
+        'user_id', 'goal', 'health', 'kg', 'study', 'thounght'
     ];
 
     public function user()
