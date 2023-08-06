55c7755c5cb42edef18491eb646ef797e3f2fcab
haru034 <haru034@icloud.com>
2023-08-06 17:34:53 +0900

    [add] jQueryで非同期通信処理を追加

---
+++ public/chat.js
@@ -0,0 +1,38 @@
+/*ホーム画面にアクセスすると5秒毎に非同期通信が実行される処理*/
+
+window.onload = function(){
+
+    function get_data() { // 非同期通信処理
+        $.ajax({
+            url: "result/ajax/",
+            dataType: "json", // 取得データをJSON形式に指定
+            success: data => {
+                $("#chat-data") // home.blade.php31行目のidに対して下記HTMLが埋め込まれる処理
+                    .find(".chat-visible")
+                    .remove();
+                console.log(data.chats);
+                for (var i = 0; i < data.chats.length; i++) {
+                    console.log(data.chats[i].name);
+                    console.log(data.chats[i].chat);
+                    var html = `
+                                <div class="media chat-visible">
+                                    <div class="media-body chat-body">
+                                        <div class="row">
+                                            <span class="chat-body-user" id="nickname">${data.chats[i].nickname}</span>
+                                            <span class="chat-body-time" id="created_at">${data.chats[i].created_at}</span>
+                                        </div>
+                                        <span class="chat-body-content" id="message">${data.chats[i].message}</span>
+                                    </div>
+                                </div>
+                            `;
+                    $("#chat-data").append(html); // 生成したHTMLコードを表示領域に追加
+                }
+            },
+            error: () => {
+                alert("非同期通信に失敗しました");
+            }
+        });
+
+        setTimeout("get_data()", 5000); // 5秒毎にget_data()の処理を実行して最新のデータを取得
+    }
+}
